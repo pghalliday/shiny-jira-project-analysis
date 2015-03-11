5cb1f33b6c97f058019cff2e93b15846d42044b4
Peter Halliday <pghalliday@gmail.com>
2015-03-11 17:14:12 +0100

    added commit info to page and ignore restart.txt

--- src/ui.R
+++ src/ui.R
@@ -1,3 +1,20 @@
+source('config.R')
+
+commitData <- {
+  repo = git2r::repository(datadir)
+  commits = git2r::commits(repo)
+  commit = commits[[1]]
+  author = commit@author
+  when = author@when
+  paste(
+    paste('date:', as.POSIXct(when@time, origin="1970-01-01")),
+    paste('name:', author@name),
+    paste('email:', author@email),
+    paste('message:', commit@message),
+    sep = '\n'
+  )
+}
+
 issuesPanel <- tabPanel(
   'Issues',
   br(),
@@ -31,6 +48,7 @@ daysPanel <- tabPanel(
 shinyUI(
   fluidPage(
     titlePanel('JIRA Project Analysis'),
+    pre(commitData),
     tabsetPanel(
       daysPanel,
       issuesPanel,
